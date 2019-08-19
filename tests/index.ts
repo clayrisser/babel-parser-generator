9f5d715cbd7889cf73e0d6cdb1b7ad66705c42ab
Jam Risser <jam@codejam.ninja>
2019-08-19 14:26:13 -0500

    50% percent coverage

--- tests/index.ts
+++ tests/index.ts
@@ -1,5 +1,68 @@
-describe('babel-parser-generator', () => {
-  it('should work', async () => {
-    expect(true).toBe(true);
+import BabelParserGenerator from '../src';
+
+describe('new BabelParserGenerator()', () => {
+  it('should create an instance', async () => {
+    expect(new BabelParserGenerator()).toMatchObject({
+      ast: {},
+      options: {}
+    });
+  });
+});
+
+describe('new BabelParserGenerator(code)', () => {
+  it('should create ast', async () => {
+    expect(new BabelParserGenerator("const hello = 'world'").ast).toMatchObject(
+      {
+        program: {
+          body: [
+            {
+              declarations: [{ id: { name: 'hello' } }]
+            }
+          ]
+        }
+      }
+    );
+  });
+});
+
+describe('new BabelParserGenerator(code).append(code)', () => {
+  it('should create ast', async () => {
+    const babelParserGenerator = new BabelParserGenerator(
+      "const hello = 'world'"
+    );
+    babelParserGenerator.append("const howdy = 'texas'");
+    expect(babelParserGenerator.ast).toMatchObject({
+      program: {
+        body: [
+          {
+            declarations: [{ id: { name: 'hello' } }]
+          },
+          {
+            declarations: [{ id: { name: 'howdy' } }]
+          }
+        ]
+      }
+    });
+  });
+});
+
+describe('new BabelParserGenerator(code).prepend(code)', () => {
+  it('should create ast', async () => {
+    const babelParserGenerator = new BabelParserGenerator(
+      "const hello = 'world'"
+    );
+    babelParserGenerator.prepend("const howdy = 'texas'");
+    expect(babelParserGenerator.ast).toMatchObject({
+      program: {
+        body: [
+          {
+            declarations: [{ id: { name: 'howdy' } }]
+          },
+          {
+            declarations: [{ id: { name: 'hello' } }]
+          }
+        ]
+      }
+    });
   });
 });
